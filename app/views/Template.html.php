32dad27a3752eba43563b9d760df8380eb801bed
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-08-07 01:01:16 +0200

    Framework's Page Template

---
+++ app/views/Template.html.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $page_title ?></title>
+    <link rel="stylesheet" href="./public/css/style.css">
+</head>
+<body>
+    <?= $page_layout ?>
+    <main>
+        <?= $page_content ?>
+    </main>
+    <script src="public/js/index.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.min.js" integrity="sha512-a6ctI6w1kg3J4dSjknHj3aWLEbjitAXAjLDRUxo2wyYmDFRcz2RJuQr5M3Kt8O/TtUSp8n2rAyaXYy1sjoKmrQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
