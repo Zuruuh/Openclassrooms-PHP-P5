b5fa8269ed29cdf613311b34fd510e55184a5f8e
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-08-07 01:01:29 +0200

    Layout Navbar

---
+++ app/views/layout/Navbar.html.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Navbar template file
+ * Navbar layout for website
+ * PHP version 8.0.9
+ *
+ * @category Layout
+ * @package  Layout
+ * @author   Younès Ziadi <ziadi.mail.pro@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     https://younes-ziadi.com/blog/
+ */
+namespace Layout;
+/**
+ * Navbar template
+ * Navbar layout for website
+ * PHP version 8.0.9
+ *
+ * @category Layout
+ * @package  Layout
+ * @author   Younès Ziadi <ziadi.mail.pro@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     https://younes-ziadi.com/blog/
+ */
+?>
+<nav>
+    Navbar !
+</nav>
