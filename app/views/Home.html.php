ff9fa23925ced4c1d44c72dde90a036aa28726c2
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-08-07 01:00:37 +0200

    Home Page View

---
+++ app/views/Home.html.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Home
+ * Main Blog page
+ * PHP version 8.0.9
+ *
+ * @category Views
+ * @package  Views
+ * @author   Younès Ziadi <ziadi.mail.pro@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     https://younes-ziadi.com/blog/
+ */
+?>
+<h1>Mon Super Blog</h1>
