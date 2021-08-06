42744db15ecee4bea7931268ee20c16c523cd757
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-08-07 01:00:17 +0200

    View file Renderer

---
+++ app/utils/Renderer.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Renderer CLass File
+ * Class for html render
+ * PHP version 8.0.9
+ *
+ * @category Utils
+ * @package  Utils
+ * @author   Younès Ziadi <ziadi.mail.pro@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     https://younes-ziadi.com/blog/
+ */
+
+namespace Utils;
+
+/**
+ * Renderer CLass
+ * Class for html render
+ * PHP version 8.0.9
+ *
+ * @category Utils
+ * @package  Utils
+ * @author   Younès Ziadi <ziadi.mail.pro@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     https://younes-ziadi.com/blog/
+ */
+
+class Renderer
+{
+    /**
+     * Renders a template file
+     * 
+     * @param string $path       Path to template file rendered
+     * @param string $page_title Page title
+     * @param array  $content    Page content & layout 
+     * @param array  $forms      All forms available on page
+     * 
+     * @return void
+     */
+    public static function render(string $path, ?string $page_title = "Blog", ?array $content = ["form_type" => "create", "values" => []], ?array $forms = null): void
+    {
+        extract($content);
+        $page_forms = array();
+        if (isset($forms)) {
+            foreach ($forms as $formClass) {
+                $form = new $formClass();
+                if (!isset($page_params)) {
+                    $page_params = [];
+                }
+                array_push($page_forms, $form->generateForm($form_type, $page_params, $values));
+            }
+        } else {
+            $page_forms = null;
+        }
+        ob_start();
+        include "app/views/" . $path . ".html.php";
+        $page_content = ob_get_clean();
+
+        ob_start();
+        include "app/views/layout/Navbar.html.php";
+        $page_layout = ob_get_clean();
+
+        include "app/views/Template.html.php";
+    }
+}
