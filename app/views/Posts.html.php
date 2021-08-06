4e9378d32510093f8f6ea812895dd0eef56a2611
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-08-07 01:01:02 +0200

    Posts Page View

---
+++ app/views/Posts.html.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Register View file
+ * Register View
+ * PHP version 8.0.9
+ *
+ * @category Views
+ * @package  Views
+ * @author   Younès Ziadi <ziadi.mail.pro@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     https://younes-ziadi.com/blog/
+ */
+
+
+foreach ($posts as $post) {
+    echo $post;
+}
+
+
+
+?>
