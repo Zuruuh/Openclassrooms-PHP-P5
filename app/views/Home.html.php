a6857e6064345379ded365bbc7a7c0221914b27f
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-08-14 09:02:53 +0200

    feat: home page

---
+++ app/views/Home.html.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Home
+ * Main Blog page
+ * PHP version 8.0.9
+ *
+ * @category Views
+ * @package  Views
+ * @author   Younès Ziadi <ziadi.mail.pro@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     https://younes-ziadi.com/blog/
+ */
+?>
+<div style="height:0;"></div>
+<h1>Mon Super Blog</h1>
+<section id="contact">
+    <?= $page_forms[0] ?>
+</section>
