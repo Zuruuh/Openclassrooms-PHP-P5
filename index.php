71d42b599df257a829abe037fdbd041650298582
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-08-14 07:35:41 +0200

    feat: use router

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Index file
+ * Main entry point for web App
+ * PHP version 8.0.9
+ *
+ * @category Utils
+ * @package  Utils
+ * @author   Younès Ziadi <ziadi.mail.pro@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     https://younes-ziadi.com/blog/
+ */
+require_once "app/App.php";
+\App::process();
