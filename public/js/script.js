521f960f4f2c7506866adc87ff7efd707045b633
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-08-14 11:45:30 +0200

    fix: added semicolons where needed

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,27 @@
 function imageInputPreview(event) {
-    const [file] = event.files
-    if(file) document.querySelectorAll('.edit-profile-picture').forEach((image) => {
-        image.src = URL.createObjectURL(file);
-    });
+    const [file] = event.files;
+    if(file) {
+        document.querySelectorAll(".edit-profile-picture").forEach((image) => {
+            image.src = URL.createObjectURL(file);
+        });
+}
 }
 
 function confirmBan(id, username) {
     const userlink = document.querySelector("#modal-ban-user");
-    userlink.href = `index.php?page=user&action=view&user=${id}`
+    userlink.href = `index.php?page=user&action=view&user=${id}`;
     userlink.innerText = username;
 
     const banbtn = document.querySelector("#modal-ban-btn");
     banbtn.href = `index.php?page=admin&action=ban&user=${id}`;
 }
 window.addEventListener("DOMContentLoaded", (e) => {
-    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+    var toastElList = [].slice.call(document.querySelectorAll(".toast"));
     var toastList = toastElList.map(function (toastEl) {
         return new bootstrap.Toast(toastEl);
     })
     toastList.map((element) => {
-        element.show();
-        })
+            element.show();
+        });
 });
 
