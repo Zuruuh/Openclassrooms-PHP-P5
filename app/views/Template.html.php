c1fe70dc3fd040371c3c845a40cd614e216fba80
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-08-14 09:52:28 +0200

    fix: escaped output & removed echos

--- app/views/Template.html.php
+++ app/views/Template.html.php
@@ -19,24 +19,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $page_title ?></title>
+    <title><?= htmlspecialchars($page_title) ?></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
     <link rel="stylesheet" href="./public/css/lib/bootstrap.min.css">
     <link rel="stylesheet" href="./public/css/style.css">
 </head>
 <body>
-    <?= $page_layout ?>
+    <?= htmlspecialchars($page_layout) ?>
     <main>
-    <?php
-    if ($page_errors !== "") {
-        $page_errors = "<section id='errors' style='z-index:1500' class='w-100 h-auto d-flex flex-column align-items-center position-fixed top-0'>" . $page_errors . "</section";
-        echo $page_errors;
-    }
-    ?>
-        <?= $page_content ?>
+    <?php if ($page_errors !== "") { ?>
+        <section id='errors' style='z-index:1500' class='w-100 h-auto d-flex flex-column align-items-center position-fixed top-0'><?= htmlspecialchars($page_errors) ?></section>";  
+    <?php } ?>
+    <?= htmlspecialchars($page_content) ?>
     </main>
     <script src="./public/js/lib/bootstrap.bundle.min.js"></script>
     <script src="./public/js/script.js"></script>
-    <?= $page_footer ?>
+    <?= htmlspecialchars($page_footer) ?>
 </body>
 </html>
