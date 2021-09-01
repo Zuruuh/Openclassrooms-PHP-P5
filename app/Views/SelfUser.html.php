e357dfe498437ba9f1f7db9bb9630fb6e2915d7b
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-01 09:25:41 +0200

    fix: changed images src folder

--- app/Views/SelfUser.html.php
+++ app/Views/SelfUser.html.php
@@ -15,9 +15,9 @@ extract($values);
 ?>
 <div style="height:0;"></div>
 <div class="w-full d-flex justify-content-center align-items-end mt-5 mb-2">
-    <img class='edit-profile-picture rounded-circle border border-1 border-primary' style="width:300px;height:300px;" src='./public/pictures/<?=$profile_picture_path?>' alt='Your profile picture'/>
-    <img class='edit-profile-picture rounded-circle border border-1 border-primary' style="width:150px;height:150px;" src='./public/pictures/<?=$profile_picture_path?>' alt='Your profile picture'/>
-    <img class='edit-profile-picture rounded-circle border border-1 border-primary' style="width:80px;height:80px;" src='./public/pictures/<?=$profile_picture_path?>' alt='Your profile picture'/>
+    <img class='edit-profile-picture rounded-circle border border-1 border-primary' style="width:300px;height:300px;" src='./public/<?=$profile_picture_path?>' alt='Your profile picture'/>
+    <img class='edit-profile-picture rounded-circle border border-1 border-primary' style="width:150px;height:150px;" src='./public/<?=$profile_picture_path?>' alt='Your profile picture'/>
+    <img class='edit-profile-picture rounded-circle border border-1 border-primary' style="width:80px;height:80px;" src='./public/<?=$profile_picture_path?>' alt='Your profile picture'/>
 </div>
 <section>
     <?php
